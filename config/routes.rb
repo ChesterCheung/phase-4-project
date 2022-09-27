c05b4cfcf6d43eb2e0e7a72bd57965a5a33522cc
Chester <cheung319@gmail.com>
2022-09-26 23:13:44 -0400

    fixed routes and added sample data

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   
   resources :assignments
   resources :hospitals
-  resources :nurses
+  # resources :nurses, only: [:create, :index]
 
+  post "/signup", to: "nurses#create"
   post "/login", to: "sessions#create"
+  get "/me", to: "nurses#show"
+  delete "/logout", to: "sessions#destroy"
+  get "/nurses", to: "nurses#index"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+nurse1 = Nurse.create!(username: "chester", password: "123")
+
+puts "Done Seeding"
