5e0b64e7dc6631461726a7ad20b56eb6f1466cc3
Chester <cheung319@gmail.com>
2022-09-26 23:14:16 -0400

    continuing to work on auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import React, {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from './components/Login'
+import Home from './components/Home'
+import NurseList from './components/NurseList'
+import AssignmentList from './components/AssignmentList'
+import AssignmentForm from './components/AssignmentForm'
+import NavBar from './components/NavBar'
+
 
 const App = () => {
   const [user, setUser] = useState(null);
 
+  useEffect(()=>{
+    fetch("/me").then((response)=> {
+      if (response.ok) {
+        response.json().then((user) => setUser(user))
+      }
+    })
+  }, []);
 
   if(!user) return <Login onLogin={setUser}/>
 
   return (
     <div>
     <Router>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/investors/:id" element={<Investors deleteTransaction={deleteTransaction} />}/>
-      <Route path="/transactions" element={<TransactionList editTransaction={editTransaction} deleteTransaction={deleteTransaction} transactions={transactions} />}/>
-      <Route path="/transactions/new" element={<TransactionForm addTransaction={addTransaction} />}/>
-    </Routes>
+      <NavBar user={user} setUser={setUser} />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/nurses" element={<NurseList />}/>
+        <Route path="/assignments" element={<AssignmentList />}/>
+        <Route path="/assignments/new" element={<AssignmentForm />}/>
+      </Routes>
   </Router>
   </div>
   )
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = ({user, setUser}) => {
+
+    const handleLogout = () => {
+        fetch("/logout", {method:"DELETE"}).then((r)=>{
+            if(r.ok) {
+                setUser(null)
+            }
+        });
+    }
+
   return (
-    <div>NavBar</div>
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/"> Home </Link></li>
+          <li><Link to="/assignments"> List Assignments </Link></li>
+          <li><Link to="/assignments/new"> Create Assignments </Link></li>
+          <button onClick={handleLogout}>Not {user}?</button>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
